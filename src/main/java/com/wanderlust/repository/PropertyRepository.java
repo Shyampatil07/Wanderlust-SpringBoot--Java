8ba451461b259fd51dd5a92f7230e2445c6c4abf
Shyampatil07 <shyamlade06@gmail.com>
2026-08-29 10:07:31 +0530

    search , filter add

--- src/main/java/com/wanderlust/repository/PropertyRepository.java
+++ src/main/java/com/wanderlust/repository/PropertyRepository.java
@@ -1,9 +1,10 @@
 package com.wanderlust.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.wanderlust.entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PropertyRepository extends JpaRepository<Property, Long > {
-
+public interface PropertyRepository
+        extends JpaRepository<Property, Long>,
+                JpaSpecificationExecutor<Property> {
 }
