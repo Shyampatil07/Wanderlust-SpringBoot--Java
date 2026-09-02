9812f40d7fb4a8d29b02b58db306ae9496511c6b
Shyampatil07 <shyamlade06@gmail.com>
2026-09-02 23:44:16 +0530

    configure file

---
+++ src/main/java/com/wanderlust/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.wanderlust.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Wanderlust API",
+                version = "1.0",
+                description = "Hotel booking REST API built with Spring Boot"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
